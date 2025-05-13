418eaab91640f89dd51081438fcbc4551e2a8b65
Igor Todorovski <itodorov@ca.ibm.com>
2025-05-12 20:43:28 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+[![Automatic version updates](https://github.com/ZOSOpenTools/doom-asciiport/actions/workflows/bump.yml/badge.svg)](https://github.com/ZOSOpenTools/doom-asciiport/actions/workflows/bump.yml)
+
+# doom-ascii
+
+doom in ascii
---
+++ cicd-stable.groovy
@@ -0,0 +1,14 @@
+node('linux')
+{
+  stage ('Poll') {
+    checkout([
+      $class: 'GitSCM',
+      branches: [[name: '*/main']],
+      doGenerateSubmoduleConfigurations: false,
+      extensions: [],
+      userRemoteConfigs: [[url: 'https://github.com/ZOSOpenTools/doom-asciiport.git']]])
+  }
+  stage('Build') {
+    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/doom-asciiport.git'), string(name: 'PORT_DESCRIPTION', value: 'doom in ascii' ), string(name: 'BUILD_LINE', value: 'STABLE') ]
+  }
+}
