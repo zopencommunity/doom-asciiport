926c17497107d377cce4737f12853123d178b952
Igor Todorovski <39890068+IgorTodorovskiIBM@users.noreply.github.com>
2025-05-12 21:21:01 -0400

    Update cicd-stable.groovy

--- cicd-stable.groovy
+++ cicd-stable.groovy
@@ -6,9 +6,9 @@ node('linux')
       branches: [[name: '*/main']],
       doGenerateSubmoduleConfigurations: false,
       extensions: [],
-      userRemoteConfigs: [[url: 'https://github.com/ZOSOpenTools/doom-asciiport.git']]])
+      userRemoteConfigs: [[url: 'https://github.com/zopencommunity/doom-asciiport.git']]])
   }
   stage('Build') {
-    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/doom-asciiport.git'), string(name: 'PORT_DESCRIPTION', value: 'doom in ascii' ), string(name: 'BUILD_LINE', value: 'STABLE') ]
+    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/zopencommunity/doom-asciiport.git'), string(name: 'PORT_DESCRIPTION', value: 'doom in ascii' ), string(name: 'BUILD_LINE', value: 'STABLE') ]
   }
 }
